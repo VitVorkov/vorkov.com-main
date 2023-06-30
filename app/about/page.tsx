5284e1f4e9220caac1990b7d6c859bb80e9baf96
Vitalii Vorkov <vitvorkov@gmail.com>
2023-06-30 16:51:15 +0200

    Remove blink of reload

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Avatar, Dropdown, Typography } from "antd";
 import { useRouter } from "next/navigation";
 import type { MenuProps } from "antd";
@@ -7,6 +8,9 @@ import type { MenuProps } from "antd";
 const { Text, Link } = Typography;
 
 export default function About() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   const router = useRouter();
 
   const items: MenuProps["items"] = [
@@ -37,77 +41,84 @@ export default function About() {
   return (
     <div
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        visibility: !mounted ? "hidden" : "visible",
+        minHeight: "100vh",
       }}
     >
       <div
         style={{
-          textAlign: "left",
-          maxWidth: "400px",
-          marginTop: "20px",
-          marginBottom: "20px",
-          backgroundColor: "white",
-          borderRadius: "10px",
-          padding: "10px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <div
           style={{
-            paddingTop: "10px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            textAlign: "left",
+            maxWidth: "400px",
+            marginTop: "20px",
+            marginBottom: "20px",
+            backgroundColor: "white",
+            borderRadius: "10px",
+            padding: "10px",
           }}
         >
-          <Avatar src={"/assets/05_Vitalii_midjourney.png"} size={128} />
+          <div
+            style={{
+              paddingTop: "10px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Avatar src={"/assets/05_Vitalii_midjourney.png"} size={128} />
+          </div>
+          <br></br>{" "}
+          <Text>
+            {" "}
+            Hi there! My name is Vitalii Vorkov and I am a software and hardware
+            engineer who is in a constant search for innovation. This is my
+            personal page and, as an experiment, I decided to make all pictures
+            for it using Midjourney. Some (if any) of my projects you can find{" "}
+            <Link href={"/projects"}>here</Link>, and I write approximately once
+            in a decade a blog post that you can read{" "}
+            <Link href={"/blog"}>here</Link>, however, the posts are in Russian.{" "}
+          </Text>{" "}
+          <br></br> <br></br>
+          <Text>
+            More information about myself can be found on{" "}
+            <Link href="https://www.linkedin.com/in/vitaliivorkov/">
+              LinkedIn
+            </Link>{" "}
+            and <Link href="https://github.com/VitVorkov">Github</Link>.{" "}
+          </Text>{" "}
+          <br></br> <br></br>
+          <Text>
+            On a personal level, I am a happy husband of Kate and a proud father
+            of Alex and Ilya. Please meet their Midjourney generated avatars
+            below.
+          </Text>{" "}
+          <br></br>
+          <div
+            style={{
+              paddingTop: "10px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Avatar src={"/assets/06_Kate_midjourney.png"} size={100} />
+            <Avatar src={"/assets/07_Alex_midjourney.png"} size={100} />
+            <Avatar src={"/assets/08_Ilya_midjourney.png"} size={100} />
+          </div>
         </div>
-        <br></br>{" "}
-        <Text>
-          {" "}
-          Hi there! My name is Vitalii Vorkov and I am a software and hardware
-          engineer who is in a constant search for innovation. This is my
-          personal page and, as an experiment, I decided to make all pictures
-          for it using Midjourney. Some (if any) of my projects you can find{" "}
-          <Link href={"/projects"}>here</Link>, and I write approximately once
-          in a decade a blog post that you can read{" "}
-          <Link href={"/blog"}>here</Link>, however, the posts are in Russian.{" "}
-        </Text>{" "}
-        <br></br> <br></br>
-        <Text>
-          More information about myself can be found on{" "}
-          <Link href="https://www.linkedin.com/in/vitaliivorkov/">
-            LinkedIn
-          </Link>{" "}
-          and <Link href="https://github.com/VitVorkov">Github</Link>.{" "}
-        </Text>{" "}
-        <br></br> <br></br>
-        <Text>
-          On a personal level, I am a happy husband of Kate and a proud father
-          of Alex and Ilya. Please meet their Midjourney generated avatars
-          below.
-        </Text>{" "}
-        <br></br>
-        <div
-          style={{
-            paddingTop: "10px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Avatar src={"/assets/06_Kate_midjourney.png"} size={100} />
-          <Avatar src={"/assets/07_Alex_midjourney.png"} size={100} />
-          <Avatar src={"/assets/08_Ilya_midjourney.png"} size={100} />
-        </div>
+        <Dropdown menu={{ items }}>
+          <Avatar
+            src={"/assets/05_Vitalii_midjourney.png"}
+            style={{ position: "absolute", top: "3%", right: "3%" }}
+          />
+        </Dropdown>
       </div>
-      <Dropdown menu={{ items }}>
-        <Avatar
-          src={"/assets/05_Vitalii_midjourney.png"}
-          style={{ position: "absolute", top: "3%", right: "3%" }}
-        />
-      </Dropdown>
     </div>
   );
 }
