afe6db05b86c64ca81efc8a458d3907f19786cab
Vitalii Vorkov <vitvorkov@gmail.com>
2023-06-30 16:48:07 +0200

    Add spinner for loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
 export default function Loading() {
-  return <p>Loading!.!</p>;
+  const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />;
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Spin indicator={antIcon}></Spin>
+    </div>
+  );
 }
