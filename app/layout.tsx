bb33eb1bcac2b52e423fc2765a01896b0d9bc381
Vitalii Vorkov <vitvorkov@gmail.com>
2023-07-28 22:14:03 +0200

    Remove Suspense on loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { Suspense } from "react";
 import "./globals.css";
 import { Roboto } from "next/font/google";
-import Loading from "./loading";
 
 const inter = Roboto({ weight: "400", subsets: ["latin"], display: "swap" });
 
@@ -17,9 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Suspense fallback={<Loading />}>{children}</Suspense>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
