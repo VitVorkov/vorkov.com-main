e96ab48eb01f013d9f6246aabb4ca7145b5aafc8
Vitalii Vorkov <vitvorkov@gmail.com>
2023-07-28 22:14:49 +0200

    Add menu component

---
+++ components/menu/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { Dropdown, Avatar } from "antd";
+import { IMainMenuProps } from "./interfaces";
+import { useRouter } from "next/navigation";
+
+export const MainMenu: FC<IMainMenuProps> = ({ items }) => {
+  const router = useRouter();
+
+  if (!items) {
+    items = [
+      {
+        key: "1",
+        label: "About",
+        onClick: async () => {
+          router.push("/about");
+        },
+      },
+      {
+        key: "2",
+        label: "Blog",
+        onClick: async () => {
+          router.push("/blog");
+        },
+        disabled: true,
+      },
+      {
+        key: "3",
+        label: "Projects",
+        onClick: async () => {
+          router.push("projects");
+        },
+        disabled: true,
+      },
+    ];
+  }
+
+  return (
+    <Dropdown menu={{ items }}>
+      <Avatar
+        src={"/assets/05_Vitalii_midjourney.png"}
+        style={{ position: "absolute", top: "3%", right: "3%" }}
+      />
+    </Dropdown>
+  );
+};
---
+++ components/menu/interfaces.tsx
@@ -0,0 +1,5 @@
+import type { MenuProps } from "antd";
+
+export interface IMainMenuProps {
+  readonly items?: MenuProps["items"];
+}
